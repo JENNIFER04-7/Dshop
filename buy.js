026b8d0f2ed4769d7ee3c2dd1d75e79d681ac632
Thin Nandar <lizzypark022@gmail.com>
2025-08-20 11:45:37 +0630

    finished

--- buy.js
+++ buy.js
@@ -1,5 +1,5 @@
 const element = document.getElementById("order");
-const myname = document.getElementById("name");
+const myname = document.getElementById("myname");
 const add = document.getElementById("add");
 
 element.addEventListener("click", myFunction);
@@ -8,6 +8,7 @@ function myFunction() {
   if (myname.value === "" || add.value === "") {
     alert("Form is empty");
   } else {
+    alert("Order placed successfully");
     window.location.assign("index.html");
 }
 }
